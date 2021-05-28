9efe93e15616f285e18b68b4e5029d80308045f5
maria-milosh <45511691+maria-milosh@users.noreply.github.com>
2021-05-28 10:58:46 -0500

    full data

--- core/analysis/topic_analysis/lda.py
+++ core/analysis/topic_analysis/lda.py
@@ -6,34 +6,39 @@ import os
 import pandas as pd
 from pprint import pprint
 
-# ADD YOURS!
-data_path = '/Users/mmilosh/Dropbox/studies/ml/RedditQuarantineNLP/BigQuery/'
 
-for file_ in os.listdir(data_path):
-    print(file_)
-    df = pd.read_csv(data_path + file_, low_memory = False)
+# data_path = '/home/mmilosh/RedditQuarantineNLP/BigQuery/'
 
-    df['body'] = df['body'].str.lower()
-    df['body'] = df['body'].apply(utils.remove_non_words)
-    print('removed non words')
-    df['tokenized_text'] = df['body'].apply(utils.word_tokenize)
-    print('tokenized text')
-    df['normalized_text'] = df['tokenized_text'].apply(utils.normalize_tokens)
-    print('normalized text')
-    # df[['body', 'tokenized_text', 'normalized_text']].head()
+# for file_ in os.listdir(data_path):
+#     print(file_)
+# df = pd.read_csv(data_path + file_, low_memory = False)
+df = pd.read_csv('/home/mmilosh/RedditQuarantineNLP/combined_bigquery_processed.csv',
+ low_memory = False, usecols = ["selftext"])
 
-    # Create corpus
-    id2word = corpora.Dictionary(df['normalized_text'])
-    corpus = [id2word.doc2bow(text) for text in df['normalized_text']]
+df['selftext'] = df['selftext'].str.lower()
+df['selftext'] = df['selftext'].apply(utils.remove_non_words)
+print('removed non words')
+df['tokenized_text'] = df['selftext'].apply(utils.word_tokenize)
+print('tokenized text')
+df['normalized_text'] = df['tokenized_text'].apply(utils.normalize_tokens)
+print('normalized text')
+df = df[['normalized_text']]
+# df[['selftext', 'tokenized_text', 'normalized_text']].head()
 
-    # Build LDA model 
-    print('building lda')
-    num_topics = 10
-    lda_model = gensim.models.LdaMulticore(corpus = corpus,
-                                        id2word = id2word,
-                                        num_topics = num_topics)
-    pprint(lda_model.print_topics())
-    doc_lda = lda_model[corpus]
+# Create corpus
+id2word = corpora.Dictionary(df['normalized_text'])
+corpus = [id2word.doc2bow(text) for text in df['normalized_text']]
 
-    modname = str('lda.model' + file_)
-    lda_model.save(modname)
+# Build LDA model 
+print('building lda')
+num_topics = 10
+lda_model = gensim.models.LdaMulticore(corpus = corpus,
+                                    id2word = id2word,
+                                    num_topics = num_topics,
+                                    workers = 30)
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]
+
+# modname = str('lda_model_' + file_)
+modname = str('lda_model')
+lda_model.save(modname)
